2ac323b5158229d5d02c8876caf0e3d73f5f996d
孟鸿飞 <mengfei0053@live.com>
2021-09-18 10:00:38 +0000

    auto redirect blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import clsx from 'clsx';
+import { withRouter } from 'react-router';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          
         </div>
       </div>
     </header>
   );
 }
 
-export default function Home(): JSX.Element {
+
+export default withRouter((props) => {
   const {siteConfig} = useDocusaurusContext();
+
+  React.useLayoutEffect(() => {
+    props.history.push('/Blog/blog')
+  },[])
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      {/* <HomepageHeader />
       <main>
         <HomepageFeatures />
-      </main>
+      </main> */}
     </Layout>
   );
-}
+})
