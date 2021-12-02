a67693d93963cdbc7ba70c2c59649679e61bbf4d
孟鸿飞 <mengfei0053@live.com>
2021-12-02 03:14:38 +0000

    修改百度推送收录api错误条件

--- scripts/push-baidu.js
+++ scripts/push-baidu.js
@@ -12,7 +12,7 @@ const urls = url.map(i => i.loc);
     try {
         const res = await axios.default.post(`http://data.zz.baidu.com/urls?site=https://blog.mengfei0053.com&token=${process.env.BAIDU_ZZ}`, urls.join('\n'));
         const data = res.data;
-        const isSuccess = typeof data.success === 'number' && typeof res.remain === 'number';
+        const isSuccess = typeof data.success === 'number' && typeof data.remain === 'number';
         if(!isSuccess){
             throw new Error(JSON.stringify(data));
         }
